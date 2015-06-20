481e1e462157e4d8a41a2f0182c2da1ce8a7e392
Sohamb <Sohamb@users.noreply.github.com>
2015-06-21 00:01:30 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions work in the same way as the makeVector() and the cachemean() functions.
+## First pass make a random square matrix and store it in a variable.
+## Then pass it through the makeCacheMatrix() function and assign it to a new variable.
+## Finally call the cacheSolve() function to use the inverse whenever needed 
+## in the same way as the solve() function is called. 
+## If the value is stored in cache...it's stored value is called
+## Else the cache is updated with the inversion matrix of the passed matrix
 
-## Write a short comment describing this function
+## This function is the function corresponding to makeVector()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) i <<- solve
+  getsolve <- function() i
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function is the function corresponding to cachemean()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getsolve()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setsolve(i)
+  i
 }
